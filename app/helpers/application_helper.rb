82ca95b1200816cab988038878ff558357af2395
Chang Ju Park <pcjpcj2@gmail.com>
2012-09-22 02:58:53 +0900

    setting Logo Title Description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,12 @@ module ApplicationHelper
 	def admin?
 		true
 	end
-  def full_title(page_title)
-    base_title = "-"
-    if page_title.empty?
+  def full_title
+    base_title = "Portolio"
+    if Setting.empty?
       base_title
     else
-      "#{page_title}"
+      base_title+" - #{page_title}"
     end
   end
   def coderay(text)
